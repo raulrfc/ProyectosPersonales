c0b07dec36d36591bce8645c8b34f310d194c8b7
raulrfc <raul.franco.ce@gmail.com>
2014-11-03 19:27:41 +0100

    Actualización
    1

---
+++ SlotMachine/SlotMachine/SlotMachine.Designer.vb
@@ -0,0 +1,327 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class SlotMachine
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.tmrSlots = New System.Windows.Forms.Timer(Me.components)
+        Me.tmrPause = New System.Windows.Forms.Timer(Me.components)
+        Me.pb1 = New System.Windows.Forms.PictureBox()
+        Me.pb2 = New System.Windows.Forms.PictureBox()
+        Me.pb3 = New System.Windows.Forms.PictureBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.PictureBox5 = New System.Windows.Forms.PictureBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.PictureBox4 = New System.Windows.Forms.PictureBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.btnSpin = New System.Windows.Forms.Button()
+        Me.Label7 = New System.Windows.Forms.Label()
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pb3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.Panel1.SuspendLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'tmrSlots
+        '
+        Me.tmrSlots.Interval = 200
+        '
+        'tmrPause
+        '
+        Me.tmrPause.Interval = 500
+        '
+        'pb1
+        '
+        Me.pb1.BackgroundImage = Global.SlotMachine.My.Resources.Resources.skull
+        Me.pb1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pb1.Location = New System.Drawing.Point(122, 198)
+        Me.pb1.Name = "pb1"
+        Me.pb1.Size = New System.Drawing.Size(116, 102)
+        Me.pb1.TabIndex = 0
+        Me.pb1.TabStop = False
+        '
+        'pb2
+        '
+        Me.pb2.BackgroundImage = Global.SlotMachine.My.Resources.Resources.presentRed
+        Me.pb2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pb2.Location = New System.Drawing.Point(278, 198)
+        Me.pb2.Name = "pb2"
+        Me.pb2.Size = New System.Drawing.Size(116, 102)
+        Me.pb2.TabIndex = 1
+        Me.pb2.TabStop = False
+        '
+        'pb3
+        '
+        Me.pb3.BackgroundImage = Global.SlotMachine.My.Resources.Resources.limon1
+        Me.pb3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pb3.Location = New System.Drawing.Point(428, 198)
+        Me.pb3.Name = "pb3"
+        Me.pb3.Size = New System.Drawing.Size(116, 102)
+        Me.pb3.TabIndex = 2
+        Me.pb3.TabStop = False
+        '
+        'Panel1
+        '
+        Me.Panel1.BackgroundImage = Global.SlotMachine.My.Resources.Resources.officeBack
+        Me.Panel1.Controls.Add(Me.Label7)
+        Me.Panel1.Controls.Add(Me.Label6)
+        Me.Panel1.Controls.Add(Me.Label5)
+        Me.Panel1.Controls.Add(Me.PictureBox5)
+        Me.Panel1.Controls.Add(Me.Label4)
+        Me.Panel1.Controls.Add(Me.PictureBox4)
+        Me.Panel1.Controls.Add(Me.Label3)
+        Me.Panel1.Controls.Add(Me.PictureBox3)
+        Me.Panel1.Controls.Add(Me.Label2)
+        Me.Panel1.Controls.Add(Me.PictureBox2)
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.PictureBox1)
+        Me.Panel1.Location = New System.Drawing.Point(0, 392)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(749, 127)
+        Me.Panel1.TabIndex = 3
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.BackColor = System.Drawing.Color.Transparent
+        Me.Label6.Font = New System.Drawing.Font("OCR A Extended", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label6.ForeColor = System.Drawing.Color.Chartreuse
+        Me.Label6.Location = New System.Drawing.Point(440, 36)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(132, 29)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "CREDITS"
+        '
+        'Label5
+        '
+        Me.Label5.BackColor = System.Drawing.Color.DarkViolet
+        Me.Label5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label5.Font = New System.Drawing.Font("Mistral", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.ForeColor = System.Drawing.Color.Bisque
+        Me.Label5.Location = New System.Drawing.Point(440, 79)
+        Me.Label5.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(195, 36)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "¡¡¡GRAN REGALO!!!"
+        '
+        'PictureBox5
+        '
+        Me.PictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox5.Image = Global.SlotMachine.My.Resources.Resources.presentGold
+        Me.PictureBox5.Location = New System.Drawing.Point(398, 78)
+        Me.PictureBox5.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox5.Name = "PictureBox5"
+        Me.PictureBox5.Size = New System.Drawing.Size(42, 36)
+        Me.PictureBox5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox5.TabIndex = 8
+        Me.PictureBox5.TabStop = False
+        '
+        'Label4
+        '
+        Me.Label4.BackColor = System.Drawing.Color.DarkRed
+        Me.Label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label4.Font = New System.Drawing.Font("Bauhaus 93", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Snow
+        Me.Label4.Location = New System.Drawing.Point(238, 78)
+        Me.Label4.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(124, 36)
+        Me.Label4.TabIndex = 7
+        Me.Label4.Text = "¡Regalo!"
+        '
+        'PictureBox4
+        '
+        Me.PictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox4.Image = Global.SlotMachine.My.Resources.Resources.presentRed
+        Me.PictureBox4.Location = New System.Drawing.Point(196, 78)
+        Me.PictureBox4.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox4.Name = "PictureBox4"
+        Me.PictureBox4.Size = New System.Drawing.Size(42, 36)
+        Me.PictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox4.TabIndex = 6
+        Me.PictureBox4.TabStop = False
+        '
+        'Label3
+        '
+        Me.Label3.BackColor = System.Drawing.Color.Black
+        Me.Label3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label3.Font = New System.Drawing.Font("Chiller", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.WhiteSmoke
+        Me.Label3.Location = New System.Drawing.Point(238, 42)
+        Me.Label3.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(124, 36)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "1 Free Murder"
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox3.Image = Global.SlotMachine.My.Resources.Resources.skull
+        Me.PictureBox3.Location = New System.Drawing.Point(196, 42)
+        Me.PictureBox3.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(42, 36)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 4
+        Me.PictureBox3.TabStop = False
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.Yellow
+        Me.Label2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label2.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.DarkGreen
+        Me.Label2.Location = New System.Drawing.Point(54, 42)
+        Me.Label2.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(114, 36)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "2 Credits"
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox2.Image = Global.SlotMachine.My.Resources.Resources.limon1
+        Me.PictureBox2.Location = New System.Drawing.Point(12, 42)
+        Me.PictureBox2.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(42, 36)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 2
+        Me.PictureBox2.TabStop = False
+        '
+        'Label1
+        '
+        Me.Label1.BackColor = System.Drawing.Color.Green
+        Me.Label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Label1.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Firebrick
+        Me.Label1.Location = New System.Drawing.Point(54, 78)
+        Me.Label1.Margin = New System.Windows.Forms.Padding(0)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(114, 36)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "1 Credit"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle
+        Me.PictureBox1.Image = Global.SlotMachine.My.Resources.Resources.cereza
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 78)
+        Me.PictureBox1.Margin = New System.Windows.Forms.Padding(0)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(42, 36)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'btnSpin
+        '
+        Me.btnSpin.BackColor = System.Drawing.Color.Green
+        Me.btnSpin.BackgroundImage = Global.SlotMachine.My.Resources.Resources.spin2
+        Me.btnSpin.FlatAppearance.BorderColor = System.Drawing.Color.YellowGreen
+        Me.btnSpin.FlatAppearance.BorderSize = 0
+        Me.btnSpin.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSpin.Font = New System.Drawing.Font("Bauhaus 93", 18.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnSpin.ForeColor = System.Drawing.Color.GreenYellow
+        Me.btnSpin.Location = New System.Drawing.Point(643, 146)
+        Me.btnSpin.Margin = New System.Windows.Forms.Padding(0)
+        Me.btnSpin.Name = "btnSpin"
+        Me.btnSpin.Size = New System.Drawing.Size(96, 103)
+        Me.btnSpin.TabIndex = 0
+        Me.btnSpin.UseVisualStyleBackColor = False
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.BackColor = System.Drawing.Color.Transparent
+        Me.Label7.Font = New System.Drawing.Font("OCR A Extended", 20.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label7.ForeColor = System.Drawing.Color.Chartreuse
+        Me.Label7.Location = New System.Drawing.Point(605, 36)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(30, 29)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "3"
+        '
+        'SlotMachine
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.White
+        Me.BackgroundImage = Global.SlotMachine.My.Resources.Resources.Skin
+        Me.ClientSize = New System.Drawing.Size(748, 518)
+        Me.Controls.Add(Me.btnSpin)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.pb3)
+        Me.Controls.Add(Me.pb2)
+        Me.Controls.Add(Me.pb1)
+        Me.Name = "SlotMachine"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Slot Machine"
+        CType(Me.pb1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pb3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        CType(Me.PictureBox5, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox4, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents tmrSlots As System.Windows.Forms.Timer
+    Friend WithEvents tmrPause As System.Windows.Forms.Timer
+    Friend WithEvents pb1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pb2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pb3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Panel1 As System.Windows.Forms.Panel
+    Friend WithEvents btnSpin As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox5 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox4 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+
+End Class
---
+++ SlotMachine/SlotMachine/SlotMachine.vb
@@ -0,0 +1,82 @@
+﻿Imports System.Windows.Forms.VisualStyles.VisualStyleElement
+
+Public Class SlotMachine
+    Private arrImages(5) As Image
+    Private RandGen As Random
+    Private RandIndex As Integer
+    Private TimeCounter As Integer
+    Private Game As Integer = 1
+    Private Credit As Integer = 3
+
+    Private Sub SlotMachine_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        arrImages(0) = My.Resources.limon1
+        arrImages(1) = My.Resources.skull
+        arrImages(2) = My.Resources.cereza
+        arrImages(3) = My.Resources.presentGold
+        arrImages(4) = My.Resources.presentRed
+
+        pb1.SizeMode = PictureBoxSizeMode.StretchImage
+        pb2.SizeMode = PictureBoxSizeMode.StretchImage
+        pb3.SizeMode = PictureBoxSizeMode.StretchImage
+    End Sub
+
+    Private Sub btnSpin_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnSpin.Click
+        RandGen = New Random(Now.Millisecond)
+        My.Computer.Audio.Play(My.Resources.Lever_Marianne_Gagnon_206547513, AudioPlayMode.WaitToComplete)
+
+        btnSpin.Enabled = False
+        tmrSlots.Enabled = True
+
+        If TimeCounter < 48 And btnSpin.Enabled = False Then
+            My.Computer.Audio.Play(My.Resources.slot, AudioPlayMode.BackgroundLoop)
+        End If     
+
+        Label7.Text = Credit
+    End Sub
+
+    Private Sub tmrSlots_Tick(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles tmrSlots.Tick
+        TimeCounter += 1
+
+        If TimeCounter <= 50 Then
+            For i = 0 To 5
+                RandIndex = RandGen.Next(0, 5)
+                pb1.Image = arrImages(RandIndex)
+                RandIndex = RandGen.Next(0, 5)
+                pb2.Image = arrImages(RandIndex)
+                RandIndex = RandGen.Next(0, 5)
+                pb3.Image = arrImages(RandIndex)
+
+            Next i
+
+            If TimeCounter = 50 Then
+                My.Computer.Audio.Stop()
+                amañar(Game)
+            End If
+        Else
+            tmrSlots.Enabled = False
+            btnSpin.Enabled = True
+            TimeCounter = 0
+            Game += 1
+            Credit -= 1
+        End If
+    End Sub
+    Private Sub amañar(ByVal juego As Integer)
+        Select Case juego
+            Case 1
+                pb1.Image = My.Resources.presentRed
+                pb2.Image = My.Resources.presentRed
+                pb3.Image = My.Resources.limon1
+            Case 2
+                pb1.Image = My.Resources.skull
+                pb2.Image = My.Resources.skull
+                pb3.Image = My.Resources.presentGold
+            Case 3
+                pb1.Image = My.Resources.presentGold
+                pb2.Image = My.Resources.presentGold
+                pb3.Image = My.Resources.presentGold
+                My.Computer.Audio.Play(My.Resources.win, AudioPlayMode.Background)
+                My.Computer.Audio.Play(My.Resources.slot_payoff, AudioPlayMode.Background)
+        End Select
+    End Sub
+End Class
+
